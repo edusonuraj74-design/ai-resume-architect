167dcaf6f633615810dd3f4778d7d473708d8021
Sonu kumar <edusonuraj74@gmail.com>
2026-06-03 12:33:02 +0530

    Added ATS score and PDF feature

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+function downloadResume() {
+    window.print();
+}
+
+function printResume() {
+    window.print();
+}
+
 function generateResume() {
 
     let name = document.getElementById("name").value;
 
-    if(name === ""){
+    if (name === "") {
         alert("Please enter your name");
         return;
     }
@@ -12,13 +20,27 @@ function generateResume() {
     let skills = document.getElementById("skills").value;
     let project = document.getElementById("project").value;
     let education = document.getElementById("education").value;
-    let photo = document.getElementById("photo").files[0];
+   /* let photo = document.getElementById("photo").files[0];
 
-   if(photo){
-    document.getElementById("showPhoto").src =
-        URL.createObjectURL(photo);
-}
+    // Photo Preview
+    if (photo) {
+        document.getElementById("showPhoto").src =
+            URL.createObjectURL(photo);
+    }*/
 
+    // ATS Score
+    let score = 0;
+
+    if (name) score += 20;
+    if (email) score += 20;
+    if (education) score += 20;
+    if (skills) score += 20;
+    if (project) score += 20;
+
+    document.getElementById("atsScore").innerHTML =
+        "ATS Score: " + score + "/100";
+
+    // Resume Preview
     document.getElementById("showName").innerHTML =
         "Name: " + name;
 
@@ -36,4 +58,23 @@ function generateResume() {
 
     document.getElementById("showProject").innerHTML =
         "Project: " + project;
+}
+
+function clearForm() {
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("education").value = "";
+    document.getElementById("skills").value = "";
+    document.getElementById("project").value = "";
+    document.getElementById("photo").value = "";
+
+    document.getElementById("showName").innerHTML = "";
+    document.getElementById("showEmail").innerHTML = "";
+    document.getElementById("showPhone").innerHTML = "";
+    document.getElementById("showEducation").innerHTML = "";
+    document.getElementById("showSkills").innerHTML = "";
+    document.getElementById("showProject").innerHTML = "";
+    document.getElementById("atsScore").innerHTML = "";
 }
