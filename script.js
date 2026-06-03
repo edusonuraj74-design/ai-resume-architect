d4edc1f6e322611a104284b781016c8fe32317d8
Sonu kumar <edusonuraj74@gmail.com>
2026-06-03 10:03:49 +0530

    Updated resume builder

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function generateResume() {
-    
+
     let name = document.getElementById("name").value;
 
     if(name === ""){
-    alert("Please enter your name");
-    return;
-}
+        alert("Please enter your name");
+        return;
+    }
 
     let email = document.getElementById("email").value;
-
     let phone = document.getElementById("phone").value;
-     
     let skills = document.getElementById("skills").value;
-
     let project = document.getElementById("project").value;
-
+    let education = document.getElementById("education").value;
 
     document.getElementById("showName").innerHTML =
         "Name: " + name;
 
     document.getElementById("showEmail").innerHTML =
-        "Email: " + email;s
+        "Email: " + email;
 
     document.getElementById("showPhone").innerHTML =
         "Phone: " + phone;
+
+    document.getElementById("showEducation").innerHTML =
+        "Education: " + education;
 
     document.getElementById("showSkills").innerHTML =
         "Skills: " + skills;
