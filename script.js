1e99f4eab308264be92de09ca691edaec4e68e83
Sonu kumar <edusonuraj74@gmail.com>
2026-06-06 12:36:58 +0530

    New features added

--- script.js
+++ script.js
@@ -8,107 +8,11 @@ function printResume() {
 
 function generateResume() {
 
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
-    let education = document.getElementById("education").value;
-    let skills = document.getElementById("skills").value;
-    let project = document.getElementById("project").value;
-    let photo = document.getElementById("photo").files[0];
+    let resume = document.querySelector(".resume");
 
-    if (name === "") {
-        alert("Please enter your name");
-        return;
-    }
-    if (email === "") {
-        alert("Please enter your email");
-        return;
-    }
-    if (phone === "") {
-        alert("Please enter your phone number");
-        return;
-    }
-    if (education === "") {
-        alert("Please enter your education");
-        return;
-    }
-    if (skills === "") {
-        alert("Please enter your skills");
-        return;
-    }
-    if (project === "") {
-        alert("Please enter your project");
-        return;
-    }
+   let template = document.getElementById("template").value;
 
-    localStorage.setItem("name", name);
-    localStorage.setItem("email", email);
-    localStorage.setItem("phone", phone);
-    localStorage.setItem("education", education);
-    localStorage.setItem("skills", skills);
-    localStorage.setItem("project", project);
-
-
-    if (photo) {
-        document.getElementById("showPhoto").src =
-            URL.createObjectURL(photo);
-    }
-
-    let score = 0;
-
-    if (name) score += 20;
-    if (email) score += 20;
-    if (education) score += 20;
-    if (skills) score += 20;
-    if (project) score += 20;
-
-    document.getElementById("atsScore").innerHTML =
-        "ATS Score: " + score + "/100";
-
-    if (score < 60) {
-        document.getElementById("atsSuggestion").innerHTML =
-            "Suggestion: Add more skills and projects.";
-    }
-    else if (score < 100) {
-        document.getElementById("atsSuggestion").innerHTML =
-            "Suggestion: Good resume, add more details.";
-    }
-    else {
-        document.getElementById("atsSuggestion").innerHTML =
-            "Suggestion: Excellent Resume!";
-    }
-
-    let strength = "";
-
-  if(score < 60){
-    strength = "Weak";
-}
-   else if(score < 100){
-    strength = "Good";
-}
-    else{
-    strength = "Excellent";
-}
-
-document.getElementById("strength").innerHTML =
-    "Resume Strength: " + strength;
-
- let skillArray = skills.split(",");
-
-document.getElementById("skillCount").innerHTML =
-    "Total Skills: " + skillArray.length;
-
-    document.getElementById("progressBar").style.width =
-    score + "%";
-
-    document.getElementById("progressBar").innerHTML =
-    score + "%";
-
-    let template = document.getElementById("template").value;
-
-   let resume = document.querySelector(".resume");
-
-    resume.classList.remove(
+resume.classList.remove(
     "simple",
     "professional",
     "modern"
@@ -126,6 +30,101 @@ resume.classList.remove(
 
 resume.classList.add(theme);
 
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let education = document.getElementById("education").value;
+    let skills = document.getElementById("skills").value;
+    let project = document.getElementById("project").value;
+    let experience = document.getElementById("experience").value;
+    let linkedin = document.getElementById("linkedin").value;
+    let github = document.getElementById("github").value;
+    let photo = document.getElementById("photo").files[0];
+
+    if (name === "") {
+        alert("Please enter your name");
+        return;
+    }
+
+    if (email === "") {
+        alert("Please enter your email");
+        return;
+    }
+
+    if (phone === "") {
+        alert("Please enter your phone number");
+        return;
+    }
+
+    if (education === "") {
+        alert("Please enter your education");
+        return;
+    }
+
+    if (skills === "") {
+        alert("Please enter your skills");
+        return;
+    }
+
+    if (project === "") {
+        alert("Please enter your project");
+        return;
+    }
+
+    // Local Storage
+    localStorage.setItem("name", name);
+    localStorage.setItem("email", email);
+    localStorage.setItem("phone", phone);
+    localStorage.setItem("education", education);
+    localStorage.setItem("skills", skills);
+    localStorage.setItem("project", project);
+    localStorage.setItem("experience", experience);
+    localStorage.setItem("linkedin", linkedin);
+    localStorage.setItem("github", github);
+
+    if (photo) {
+        document.getElementById("showPhoto").src =
+            URL.createObjectURL(photo);
+    }
+
+    // ATS Score
+    let score = 0;
+
+    if (name) score++;
+    if (email) score++;
+    if (education) score++;
+    if (skills) score++;
+    if (project) score++;
+    if (experience) score++;
+
+    score = Math.round((score / 6) * 100);
+
+    document.getElementById("atsScore").innerHTML =
+        "ATS Score: " + score + "/100";
+        let strength = "";
+
+if(score < 60){
+    strength = "Weak";
+}
+else if(score < 100){
+    strength = "Good";
+}
+else{
+    strength = "Excellent";
+}
+
+document.getElementById("strength").innerHTML =
+    "Resume Strength: " + strength;
+
+let skillArray = skills.split(",");
+
+document.getElementById("skillCount").innerHTML =
+    "Total Skills: " + skillArray.length;
+
+document.getElementById("atsSuggestion").innerHTML =
+    "Suggestion: Add more skills and projects.";
+
+    // Preview
     document.getElementById("showName").innerHTML =
         "Name: " + name;
 
@@ -143,6 +142,15 @@ resume.classList.add(theme);
 
     document.getElementById("showProject").innerHTML =
         "Project: " + project;
+
+    document.getElementById("showExperience").innerHTML =
+        "Experience: " + experience;
+
+    document.getElementById("showLinkedin").innerHTML =
+        "LinkedIn: " + linkedin;
+
+    document.getElementById("showGitHub").innerHTML =
+        "GitHub: " + github;
 }
 
 function clearForm() {
@@ -154,6 +162,14 @@ function clearForm() {
     document.getElementById("skills").value = "";
     document.getElementById("project").value = "";
     document.getElementById("photo").value = "";
+    document.getElementById("github").value = "";
+    document.getElementById("linkedin").value = "";
+    document.getElementById("experience").value = "";
+    
+
+
+   
+
 
     document.getElementById("showName").innerHTML = "";
     document.getElementById("showEmail").innerHTML = "";
@@ -164,6 +180,10 @@ function clearForm() {
     document.getElementById("atsScore").innerHTML = "";
     document.getElementById("atsSuggestion").innerHTML = "";
     document.getElementById("skillCount").innerHTML = "";
+    document.getElementById("showLinkedin").innerHTML = "";
+    document.getElementById("showGitHub").innerHTML = "";
+    document.getElementById("showExperience").innerHTML = "";
+    document.getElementById("strength").innerHTML = "";
 
     localStorage.clear();
 
@@ -195,4 +215,13 @@ window.onload = function() {
 
     document.getElementById("project").value =
         localStorage.getItem("project") || "";
+
+    document.getElementById("experience").value =
+        localStorage.getItem("experience") || "";
+
+    document.getElementById("linkedin").value =
+        localStorage.getItem("linkedin") || "";
+
+    document.getElementById("github").value =
+        localStorage.getItem("github") || "";
 }
